e2e72ce79b3b726fb3b42abff2f7c53516a52727
kamimiller <mrkamimiller@outlook.com>
2022-12-29 23:45:23 +0330

    complate form redeem to file models.py

--- sms_bomber/forms.py
+++ sms_bomber/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, PasswordField
+from wtforms import IntegerField, StringField, PasswordField, DateField
 from wtforms.validators import DataRequired
 
 class EnterNumber(FlaskForm):
@@ -9,3 +9,7 @@ class EnterNumber(FlaskForm):
 class Login(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    
+class Redeem(FlaskForm):
+    Redeem = StringField('Redeem', validators=[DataRequired()])
+    date_end = DateField('Date', validators=[DataRequired()])
